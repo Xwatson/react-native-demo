31f7a3e064895f55c940e0134f0f3c36e5647ca8
xwatson <xwatson@foxmail.com>
2018-11-20 19:35:26 +0800

    add mobo store

--- App.js
+++ App.js
@@ -7,17 +7,21 @@
  */
 
 import React, {Component} from 'react';
+import { Provider } from "mobx-react";
 
 import { createStackNavigator } from 'react-navigation'
 import RouteConfig from './src/router/RouterConfig'
 import StackNavigatorConfig from './src/router/StackNavigatorConfig'
+import store from "./src/store";
 
 const Navigator = createStackNavigator(RouteConfig, StackNavigatorConfig);
 
 export default class App extends Component {
   render() {
     return (
-      <Navigator />
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
     )
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { View, Text } from "react-native";
+import { inject, observer } from "mobx-react/native";
 import Header from '../components/Header';
 
+@inject('homeStore')
+@observer // 监听当前组件
 export default class Home extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         tabBarLabel: '首页'
     })
+    constructor(props) {
+        super(props);
+        this.homeStore = this.props.homeStore
+    }
+
     render() {
+        const { text = '123', num =0 } = this.homeStore;
         return (
             <View>
                 <Header title={'首页'} />
-                <Text>我是首页</Text>
+                <Text>{text}</Text>
+                <Button primary onPress={() => this.homeStore.plus()}>
+                    <Text>add</Text>
+                </Button>
+                <Text>{num}</Text>
+                <Button primary onPress={() => this.homeStore.minus()}>
+                    <Text>Minu</Text>
+                </Button>
             </View>
         )
     }

696fb5df573e721080b401945326f945152fb8b9
xwatson <xwatson@foxmail.com>
2018-12-04 15:43:19 +0800

    取消阿里推送测试代码

--- src/views/GoodsReptile/GoodsReptileList.js
+++ src/views/GoodsReptile/GoodsReptileList.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import { View, Text, Button, NativeModules } from "react-native";
+import { View, Text, Button } from "react-native";
 import Header from "../../components/Header";
-const mPush = NativeModules.MPush;
 
 export default class GoodsReptileList extends React.Component {
     static navigationOptions = ({ navigation }) => ({
@@ -9,9 +8,6 @@ export default class GoodsReptileList extends React.Component {
     })
     constructor(props) {
         super(props);
-        this.state = {
-            deviceIdBtnTitle: ''
-        }
     }
     render() {
         return (
@@ -23,16 +19,6 @@ export default class GoodsReptileList extends React.Component {
                     onPress={() => {
                         this.props.navigation.navigate('GoodsReptileDetail', {id: 123});
                     }} />
-                <Button
-                    title='MPush调用Native方法'
-                    onPress={() => {
-                        mPush.getDeviceId((args) => {
-                            this.setState({
-                                deviceIdBtnTitle: args
-                            });
-                        });
-                    }} />
-                <Text>getDeviceId：{this.state.deviceIdBtnTitle}</Text>
             </View>
         )
     }

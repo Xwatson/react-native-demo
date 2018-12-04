f904259b2c1dda150d4e5feb669e61ed60aa160e
xwatson <xwatson@foxmail.com>
2018-12-04 15:39:23 +0800

    添加信鸽推送支持

--- App.js
+++ App.js
@@ -10,45 +10,135 @@ import React, { Component } from 'react';
 import { Provider } from "mobx-react";
 
 import { createStackNavigator } from 'react-navigation'
-import { DeviceEventEmitter, NativeModules } from 'react-native'
+import { Platform, DeviceEventEmitter } from 'react-native'
 import RouteConfig from './src/router/RouterConfig'
 import StackNavigatorConfig from './src/router/StackNavigatorConfig'
 import store from "./src/store";
 import './src/utils/storage';
-const mPush = NativeModules.MPush;
+import XGPush from 'react-native-xinge-push';
 
 const Navigator = createStackNavigator(RouteConfig, StackNavigatorConfig);
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDebug: false
+    };
+    this._enableDebug = this._enableDebug.bind(this);
+    this._isEnableDebug = this._isEnableDebug.bind(this);
+
+    // 初始化推送
+    this.initPush();
+  }
+
+  initPush() {
+    // 初始化
+    if(Platform.OS === 'android') {
+      // 请将1111111111修改为APP的AccessId，10位数字
+      // 请将YOUR_ACCESS_KEY修改为APP的AccessKey
+      XGPush.init(1111111111, 'YOUR_ACCESS_KEY');
+    } else {
+      // 请将1111111111修改为APP的AccessId，10位数字
+      // 请将YOUR_ACCESS_KEY修改为APP的AccessKey
+      XGPush.init(1111111111, 'YOUR_ACCESS_KEY');
+    }
+
+    // XGPush.setHuaweiDebug(true);
+
+    // 小米
+    XGPush.initXiaomi('AppID', 'AppKey');
+
+    // 魅族
+    XGPush.initMeizu('appId', 'appKey');
+
+    // 华为请到 build.gradle manifestPlaceholders 配置
+
+    // 第三方推送开关（华为、小米、魅族）
+    XGPush.enableOtherPush(true);
+
+    // 注册
+    XGPush.register('xwatson')
+      .then(result => {
+        // do something
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   componentDidMount() {
-    //绑定账号 
-    mPush.bindAccount('xwatson@foxmail.com',(bindCallback)=>{
-      //绑定回调结果
-      alert(bindCallback);
-    });
-    DeviceEventEmitter.addListener('onMessage', this.onMessage);
-    DeviceEventEmitter.addListener('onNotification', this.onNotification);
-    DeviceEventEmitter.addListener('onNotificationOpened', this.onNotificationOpened);
-    DeviceEventEmitter.addListener('onNotificationRemoved', this.onNotificationRemoved);
+    XGPush.addEventListener('register', this._onRegister);
+    XGPush.addEventListener('message', this._onMessage);
+    XGPush.addEventListener('notification', this._onNotification);
   }
 
   componentWillUnmount() {
-    DeviceEventEmitter.removeListener('onMessage', this.onMessage);
-    DeviceEventEmitter.removeListener('onNotification', this.onNotification);
-    DeviceEventEmitter.removeListener('onNotificationOpened', this.onNotificationOpened);
-    DeviceEventEmitter.removeListener('onNotificationRemoved', this.onNotificationRemoved);
+    XGPush.removeEventListener('register', this._onRegister);
+    XGPush.removeEventListener('message', this._onMessage);
+    XGPush.removeEventListener('notification', this._onNotification);
   }
-  onMessage(e) {
-    alert("Message Received. Title:" + e.title + ", Content:" + e.content);
+
+  /**
+   * 注册成功
+   * @param deviceToken
+   * @private
+   */
+  _onRegister(deviceToken) {
+    alert('onRegister: ' + deviceToken);
   }
-  onNotification(e) {
-    alert("Notification Received.Title:" + e.title + ", Content:" + e.content);
+
+  /**
+   * 透传消息到达
+   * @param message
+   * @private
+   */
+  _onMessage(message) {
+    alert('收到透传消息: ' + message.content);
   }
-  onNotificationOpened(e) {
-    alert("Notification Clicked");
+
+  /**
+   * 通知到达
+   * @param notification
+   * @private
+   */
+  _onNotification(notification) {
+    if(notification.clicked === true) {
+      alert('app处于后台时收到通知' + JSON.stringify(notification));
+    } else {
+      alert('app处于前台时收到通知' + JSON.stringify(notification));
+    }
   }
-  onNotificationRemoved(e) {
-    alert("Notification removed");
+
+  /**
+   * 获取初始通知（点击通知后）
+   * @private
+   */
+  _getInitialNotification() {
+    XGPush.getInitialNotification().then((result) => {
+      alert(JSON.stringify(result));
+    });
+  }
+
+  _enableDebug() {
+    XGPush.enableDebug(!this.state.isDebug);
+  }
+
+  _isEnableDebug() {
+    XGPush.isEnableDebug().then(result => {
+      this.setState({
+        isDebug: result
+      });
+      alert(result);
+    });
+  }
+
+  _setApplicationIconBadgeNumber(number = 0) {
+    XGPush.setApplicationIconBadgeNumber(number);
+  }
+
+  _getApplicationIconBadgeNumber() {
+    XGPush.getApplicationIconBadgeNumber((number) => alert(number));
   }
   render() {
     return (
--- android/app/src/main/java/com/mytools_rn/MainApplication.java
+++ android/app/src/main/java/com/mytools_rn/MainApplication.java
@@ -2,11 +2,10 @@ package com.mytools_rn;
 
 import android.app.Application;
 
-import com.alibaba.sdk.android.push.CommonCallback;
-import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
-import com.mytools_rn.push.PushModule;
-import com.mytools_rn.push.PushPackage;
+// import com.mytools_rn.push.PushModule;
+// import com.mytools_rn.push.PushPackage;
 import com.facebook.react.ReactApplication;
+import com.jeepeng.react.xgpush.PushPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -52,25 +51,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    this.initCloudChannel();
-  }
-  private void initCloudChannel() {
-    PushServiceFactory.init(this.getApplicationContext());
-    PushServiceFactory.getCloudPushService().register(this.getApplicationContext(), new CommonCallback() {
-      @Override
-      public void onSuccess(String s) {
-        WritableMap params = Arguments.createMap();
-        params.putBoolean("success", true);
-        PushModule.sendEvent("onInit", params);
-      }
-
-      @Override
-      public void onFailed(String s, String s1) {
-        WritableMap params = Arguments.createMap();
-        params.putBoolean("success", false);
-        params.putString("errorMsg", "errorCode:" + s + ". errorMsg:" + s1);
-        PushModule.sendEvent("onInit", params);
-      }
-    });
+    // this.initCloudChannel();
   }
 }

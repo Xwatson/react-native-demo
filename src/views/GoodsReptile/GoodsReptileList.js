b16084a857ec5bf4827820a1420649dcdf6ef836
xwatson <xwatson@foxmail.com>
2018-12-12 16:16:11 +0800

    修改列表

--- src/views/GoodsReptile/GoodsReptileList.js
+++ src/views/GoodsReptile/GoodsReptileList.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { View, Text, Button, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Header from "../../components/Header";
-import { List } from 'antd-mobile-rn';
+import Icon from "react-native-vector-icons/Feather";
+import { List, Tag } from 'antd-mobile-rn';
 
 const Item = List.Item;
 const Brief = Item.Brief;
@@ -12,105 +13,126 @@ export default class GoodsReptileList extends React.Component {
     })
     constructor(props) {
         super(props);
+        this.data = [
+            {
+                id: 1,
+                name: '商品名称aaaa', // 商品名称
+                site_name: '天猫', // 站点名称
+                image_url: '', // 图片地址
+                current_price: 21, // 当前价格
+                vip_price: 19, // 会员价
+                lowest_price: 19, // 最低价
+                expect_price: 10, // 期望价
+                code: 0
+            },
+            {
+                id: 2,
+                name: '商品名称bbbb', // 商品名称
+                site_name: '京东', // 站点名称
+                image_url: '', // 图片地址
+                current_price: 98, // 当前价格
+                vip_price: 98, // 会员价
+                lowest_price: 98, // 最低价
+                expect_price: 78, // 期望价
+                code: 1
+            }
+        ]
+    }
+    renderListItem = (data = []) => {
+        return data.map(item => 
+            <Item key={item.id} thumb={item.image_url || 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png'}
+                arrow="horizontal"
+                multipleLine
+                onClick={() => this.props.navigation.navigate('GoodsReptileDetail', {id: item.id})}
+            >
+                {item.name}
+                <View style={styles.listItemBrief}>
+                    <View style={[styles.listItemBriefBox, styles.listItemBriefSite]}><Text style={{ color: '#fff' }}>{item.site_name}</Text></View>
+                    {this.getPriceStatusIcon(item)}
+                    <View style={[styles.listItemBriefBox, styles.listItemBriefVipPrice]}><Text style={{color: '#e4c06a'}}>vip:{item.vip_price}</Text></View>
+                    <View style={[styles.listItemBriefBox, styles.listItemBriefExpectPrice]}><Text style={{color: '#090'}}>期望:{item.expect_price}</Text></View>
+                    <View style={[styles.listItemBriefBox, styles.listItemBriefLowestPrice]}><Text style={{color: 'red'}}>最低:{item.lowest_price}</Text></View>
+                    {
+                        item.code ? 
+                            <View style={[styles.listItemBriefBox, styles.listItemBriefCode]}><Text style={{color: '#fff'}}>Error</Text></View> :
+                            <Text/>
+                    }
+                </View>
+            </Item>
+            )
+    }
+    getPriceStatusIcon = (item = {}) => {
+        if (item.current_price > item.expect_price) {
+            return (
+                <View style={[styles.listItemBriefBox]}>
+                    <Text style={{ color: '#f54545' }}>{item.current_price}<Icon name={'arrow-up'} size={10} color={'#f54545'} /></Text>
+                </View>
+            )
+        } else {
+            return (
+                <View style={[styles.listItemBriefBox]}>
+                    <Text style={{ color: '#090' }}>{item.current_price}<Icon name={'arrow-down'} size={10} color={'#090'} /></Text>
+                </View>
+            )
+        }
     }
     render() {
         return (
-            <View>
+            <View style={{ flex: 1 }}>
                 <Header title={'商品爬虫'} rightButton={true} />
                 <ScrollView
-                style={{ backgroundColor: '#f5f5f9' }}
-                >
-                    
-                    <Text>我是列表页</Text>
-                    <Button
-                        title='详情页'
-                        onPress={() => {
-                            this.props.navigation.navigate('GoodsReptileDetail', {id: 123});
-                    }} />
+                    style={{ flex: 1, backgroundColor: '#f5f5f9' }} >
                     <List renderHeader={'basic'}>
-                        <Item data-seed="logId">
-                        标题文字点击无反馈，文字超长则隐藏，文字超长则隐藏
-                        </Item>
-                        <Item wrap>
-                        文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行
-                        </Item>
-                        <Item disabled extra="箭头向右" arrow="horizontal" onPress={() => {}}>
-                        标题文字
-                        </Item>
-                        <Item extra="箭头向下" arrow="down" onPress={() => {}}>
-                        标题文字
-                        </Item>
-                        <Item extra="箭头向上" arrow="up" onPress={() => {}}>
-                        标题文字
-                        </Item>
-                        <Item extra="没有箭头" arrow="empty">
-                        标题文字
-                        </Item>
-                        <Item
-                            extra={
-                                <View>
-                                内容内容
-                                <Brief style={{ textAlign: 'right' }}>辅助文字内容</Brief>
-                                </View>
-                            }
-                            multipleLine
-                        >
-                        垂直居中对齐
-                        </Item>
-                        <Item extra="内容内容" multipleLine>
-                        垂直居中对齐<Brief>辅助文字内容</Brief>
-                        </Item>
-                        <Item
-                        wrap
-                        extra="文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行"
-                        multipleLine
-                        align="top"
-                        arrow="horizontal"
-                        >
-                        顶部对齐
-                        <Brief>辅助文字内容辅助文字内容辅助文字内容辅助文字内容</Brief>
-                        <Brief>辅助文字内容</Brief>
-                        </Item>
-                        <Item
-                        extra={
-                            <View>
-                            内容内容
-                            <Brief style={{ textAlign: 'right' }}>辅助文字内容</Brief>
-                            </View>
-                        }
-                        multipleLine
-                        align="bottom"
-                        >
-                        底部对齐
-                        </Item>
-                    </List>
-                    <List renderHeader={'带缩略图'}>
-                        <Item thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png">
-                        thumb
-                        </Item>
-                        <Item
-                        thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-                        arrow="horizontal"
-                        >
-                        thumb
-                        </Item>
-                        <Item
-                        extra={
-                            <Image
-                            source={{
-                                uri:
-                                'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
-                            }}
-                            style={{ width: 29, height: 29 }}
-                            />
-                        }
-                        arrow="horizontal"
-                        >
-                        extra为Image
-                        </Item>
+                        {this.renderListItem(this.data)}
                     </List>
                 </ScrollView>
             </View>
           );
     }
 }
+
+const styles = StyleSheet.create({
+    listItemBrief: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    listItemBriefBox: {
+        paddingHorizontal: 2,
+        paddingVertical: 1,
+        marginHorizontal: 2,
+        marginVertical: 0,
+        fontSize: 12,
+        borderRadius: 3,
+    },
+    listItemBriefSite: {
+        marginLeft: 0,
+        color: '#fff',
+        backgroundColor: '#f54545',
+    },
+    listItemBriefVipPrice: {
+        color: '#e4c06a',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#e4c06a',
+    },
+    listItemBriefExpectPrice: {
+        color: '#090',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#090',
+    },
+    listItemBriefLowestPrice: {
+        color: 'red',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: 'red',
+    },
+    listItemBriefCode: {
+        color: '#fff',
+        fontWeight:'bold',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#e4393c',
+        backgroundColor: '#e4393c',
+    },
+})
